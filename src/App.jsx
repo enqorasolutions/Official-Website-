c265ea47d4657444c25362db6b15e253aecb92c6
Disha <dishavyas263@gmail.com>
2026-08-25 11:59:34 +0530

    Fix ScrollToTop import case

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import ScrollToTop from "./components/Scrolltotop.jsx";
+import ScrollToTop from "./components/ScrollToTop";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
